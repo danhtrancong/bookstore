a28ee8e2e1b558a0b4adef5ee37ed094b09d8212
bao linh <vuamattroi1999@gmail.com>
2020-10-14 21:03:47 +0700

    [ BE_0003 ] : Added the configuration for spring security

---
+++ bookstore/src/main/java/com/bookstore/configuration/SecurityConfiguration.java
@@ -0,0 +1,84 @@
+package com.bookstore.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	/*
+	 * UserDetailsService la interface cua spring security UserDetails
+	 * loadUserByUsername(String username) throws UsernameNotFoundException minh can
+	 * phai impl method nay de query user tu database tai sao su dung
+	 * UserDetailsService iterface ?
+	 */
+
+	@Autowired
+	@Qualifier("customUserDetailsService")
+	UserDetailsService userDetailsService;
+
+	// TODO : tai sao khong qualifier ?
+	@Autowired
+	PersistentTokenRepository tokenRepository;
+
+	// khong hieu
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/home", "/list").access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
+		.antMatchers("/newuser/**", "/delete-user-*").access("hasRole('ADMIN')")
+		.antMatchers("/edit-user-*").access("hasRole('ADMIN') or hasRole('DBA')")
+		.and()
+		.formLogin().loginPage("/login").loginProcessingUrl("/api-login").usernameParameter("username").passwordParameter("password")
+		.and()
+		.rememberMe().rememberMeParameter("remember-me")
+		.tokenRepository(tokenRepository).tokenValiditySeconds(86400)
+		.and().csrf().and().exceptionHandling().accessDeniedPage("/access_denied");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	 @Bean
+	    public DaoAuthenticationProvider authenticationProvider() {
+	        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+	        authenticationProvider.setUserDetailsService(userDetailsService);
+	        authenticationProvider.setPasswordEncoder(passwordEncoder());
+	        return authenticationProvider;
+	    }
+	 
+	    @Bean
+	    public PersistentTokenBasedRememberMeServices getPersistentTokenBasedRememberMeServices() {
+	        PersistentTokenBasedRememberMeServices tokenBasedservice = new PersistentTokenBasedRememberMeServices(
+	                "remember-me", userDetailsService, tokenRepository);
+	        return tokenBasedservice;
+	    }
+	 
+	    @Bean
+	    public AuthenticationTrustResolver getAuthenticationTrustResolver() {
+	        return new AuthenticationTrustResolverImpl();
+	    }
+}
